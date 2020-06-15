d309085b4d486febf5f931ed1ba06e32f5caa608
kevingomezcantillo@hotmail.com <kevingomezcantillo@hotmail.com>
2020-06-15 11:16:16 -0500

    COMMIT 11:16 AM

---
+++ DAL/RecaudoRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using Entity;
+namespace DAL
+{
+    public class RecaudoRepository
+    {
+        public List<Recaudo> recaudos = new List<Recaudo>();
+        private readonly SqlConnection _connection;
+        public RecaudoRepository(ConnectionManager connection)
+        {
+            _connection = connection._conexion;
+            recaudos = new List<Recaudo>();
+        }
+
+        public void GuardarArchivoPlano()
+        {
+
+
+            using (var command = _connection.CreateCommand())
+            {
+             //   command.CommandText = "BULK INSERT DetalleOrden FROM 'C:\\Users\\Gomez\\source\\repos\\KeedSupport UI\\KeedSupport UI\\KeedSupport UI\\KeedSupport UI\\bin\\Debug\\Detalle.txt' WITH (FIELDTERMINATOR = ';' , ROWTERMINATOR = '\n')";
+
+                command.CommandText = "BULK INSERT DetalleOrden FROM 'C:\\Users\\Gomez\\source\\repos\\Gobernacion del Magdalena\\Recaudo.txt' WITH (FIELDTERMINATOR = ';' , ROWTERMINATOR = '\n')";
+                command.CommandType = CommandType.Text;
+
+                command.ExecuteNonQuery();
+                command.Parameters.Clear();
+            }
+
+        }
+
+    }
+    
+
+}
