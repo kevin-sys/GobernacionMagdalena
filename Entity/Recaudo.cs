4543c308a61491b8fb99d7a0f014ca89aaf62cfd
kevingomezcantillo@hotmail.com <kevingomezcantillo@hotmail.com>
2020-06-15 11:05:48 -0500

    COMMIT 11:05

---
+++ Entity/Recaudo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Recaudo
+    {
+        public string NitAgenteRecaudador { get; set; }
+        public int MesReporte { get; set; }
+        public int AñoReporte { get; set; }
+        public string TipoEstampilla { get; set; }
+        public decimal ValorImpuesto { get; set; }
+        public string IdentificacionContratista { get; set; }
+        public string NombreContratista { get; set; }
+
+    }
+}
--- Gobernacion del Magdalena/Form1.Designer.cs
+++ Gobernacion del Magdalena/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.TxtNitAgente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Location = new System.Drawing.Point(167, 125);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(142, 23);
+            this.BtnSalvar.TabIndex = 0;
+            this.BtnSalvar.Text = "CARGAR ARCHIVO";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // TxtNitAgente
+            // 
+            this.TxtNitAgente.Location = new System.Drawing.Point(193, 99);
+            this.TxtNitAgente.Name = "TxtNitAgente";
+            this.TxtNitAgente.Size = new System.Drawing.Size(100, 20);
+            this.TxtNitAgente.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NIT AGENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Gobernación del Magdalena";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(468, 222);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtNitAgente);
+            this.Controls.Add(this.BtnSalvar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.TextBox TxtNitAgente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
